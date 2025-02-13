59f9cbf8c31860de98b9b1578af94ac52c082e50
Radvil <radvil.linux@gmail.com>
2025-02-13 20:25:55 +0800

    Revert brave install

--- installers/desktop/install.sh
+++ installers/desktop/install.sh
@@ -10,7 +10,3 @@ dnf5 install -y vinyl-theme breeze-gtk gparted
 # Install custom plymouth theme
 dnf5 -y install plymouth-theme-solar && \
 	plymouth-set-default-theme solar
-
-# Install web browser
-dnf5 config-manager --add-repo https://brave-browser-rpm-release.s3.brave.com/brave-browser.repo -y
-dnf5 install -y brave-browser
