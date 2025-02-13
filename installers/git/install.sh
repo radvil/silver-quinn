afb3296570cd2ccbf1f659c075ccd9fb9a3173dc
Radvil <radvil.linux@gmail.com>
2025-02-13 16:18:55 +0800

    Remove user's git config

--- installers/git/install.sh
+++ installers/git/install.sh
@@ -5,18 +5,6 @@ set -ouex pipefail
 # Install git
 dnf5 install -y git gh
 
-# Configure git
-git config --global user.name "Radvil"
-git config --global user.email "radvil.linux@gmail.com"
-git config --global core.editor "nvim"
-git config --global color.ui true
-
-# Configure gh
-gh config set editor "nvim"
-gh config set git_protocol "ssh"
-gh config set prompt "disabled"
-gh config set editor "nvim"
-
 # Install LazyGit from Copr
 dnf5 copr enable atim/lazygit -y
 dnf5 install lazygit
