38a04bd67c598e14bbbd667c916d563928dbcff1
Radvil <radvil.linux@gmail.com>
2025-02-13 17:30:11 +0800

    fix: remove gnome-session

--- installers/desktop/install.sh
+++ installers/desktop/install.sh
@@ -2,25 +2,6 @@
 
 set -ouex pipefail
 
-# Install Gnome Session Packages
-dnf5 -y install \
-    nautilus-gsconnect \
-    gnome-shell \
-    gnome-shell-extension-appindicator \
-    gnome-shell-extension-user-theme \
-    gnome-shell-extension-gsconnect \
-    gnome-shell-extension-blur-my-shell \
-    gnome-shell-extension-caffeine \
-    firewall-config
-
-# Remove unnecessary gnome packages
-dnf5 -y remove \
-    gnome-classic-session \
-    gnome-tour \
-    gnome-extensions-app \
-    gnome-system-monitor \
-    gnome-initial-setup
-
 # Install KDEklassy theme
 wget -O Klassy.rpm https://download.opensuse.org/repositories/home:/paul4us/Fedora_41/x86_64/klassy-6.2.breeze6.2.1-10.2.x86_64.rpm
 dnf install -y Klassy.rpm
