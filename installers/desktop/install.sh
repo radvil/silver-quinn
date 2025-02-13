dabb6ca7251e224178484eea07a81e4e4b60aa9b
Radvil <radvil.linux@gmail.com>
2025-02-13 20:11:48 +0800

    Add default system web browser

--- installers/desktop/install.sh
+++ installers/desktop/install.sh
@@ -3,11 +3,15 @@
 set -ouex pipefail
 
 # Install KDE vinyl theme
-dnf copr enable -y ekaaty/kde-extras
+dnf5 copr enable -y ekaaty/kde-extras
 
 dnf5 install -y vinyl-theme breeze-gtk gparted
 
 # Install custom plymouth theme
-dnf -y install plymouth-theme-solar && \
+dnf5 -y install plymouth-theme-solar && \
 	plymouth-set-default-theme solar && \
 	plymouth-set-default-theme -R
+
+# Install web browser
+dnf5 config-manager --add-repo https://brave-browser-rpm-release.s3.brave.com/brave-browser.repo -y
+dnf5 install -y brave-browser
