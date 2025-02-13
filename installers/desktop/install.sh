afe6a280398daad2aab752214ac6ad9ed57b54b0
Radvil <radvil.linux@gmail.com>
2025-02-13 19:25:23 +0800

    Remove klassy, add more desktop packages

--- installers/desktop/install.sh
+++ installers/desktop/install.sh
@@ -2,11 +2,7 @@
 
 set -ouex pipefail
 
-# Install KDEklassy theme
-wget -O Klassy.rpm https://download.opensuse.org/repositories/home:/paul4us/Fedora_41/x86_64/klassy-6.2.breeze6.2.1-10.2.x86_64.rpm
-dnf install -y Klassy.rpm
-rm -rf Klassy.rpm
-
 # Install KDE vinyl theme
 dnf copr enable -y ekaaty/kde-extras
-dnf install -y vinyl-theme
+
+dnf5 install -y vinyl-theme breeze-gtk gparted
