67633e719d62d63da5592fcc95f2dc1dba6e7fac
Radvil <radvil.linux@gmail.com>
2025-02-17 04:02:01 +0800

    remove brave install because /opt/brave won't work on silverblue

--- installers/desktop/install.sh
+++ installers/desktop/install.sh
@@ -7,6 +7,6 @@ dnf5 -y install plymouth-theme-solar &&
   plymouth-set-default-theme solar
 
 # Install brave browser
-dnf5 install -y 'dnf-command(config-manager)'
-dnf5 config-manager addrepo -y --overwrite --from-repofile=https://brave-browser-rpm-release.s3.brave.com/brave-browser.repo
-dnf5 install -y brave-browser --refresh
+# dnf5 install -y 'dnf-command(config-manager)'
+# dnf5 config-manager addrepo -y --overwrite --from-repofile=https://brave-browser-rpm-release.s3.brave.com/brave-browser.repo
+# dnf5 install -y brave-browser --refresh
