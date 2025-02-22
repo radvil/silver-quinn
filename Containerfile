2bd1f7c6f13c28ae092ef916956c17f09e6a58f5
Radvil <radvil.linux@gmail.com>
2025-02-22 17:11:41 +0800

    init experimental changes

--- Containerfile
+++ Containerfile
@@ -1,10 +1,16 @@
-# Using bazzite-gnome-asus-nvidia as base image
+# Using either bazzite-asus-nvidia or bazzite-gnome-asus-nvidia as base image
 # renovate: datasource=github-releases depName=ublue-os/bazzite
 
+# Default to bazzite-asus-nvidia if not specified
+# To use GNOME variant, set BASE_IMAGE=bazzite-gnome-asus-nvidia
 ARG BASE_IMAGE="${BASE_IMAGE:-bazzite-asus-nvidia}"
 ARG RELEASE_TAG="${RELEASE_TAG:-stable}"
 
 FROM ghcr.io/ublue-os/${BASE_IMAGE}:${RELEASE_TAG} as silverquinn
+
+# Set environment variable for build scripts to detect GNOME variant
+ENV IS_GNOME_VARIANT=0
+ENV IS_GNOME_VARIANT=${BASE_IMAGE:+${BASE_IMAGE#*bazzite-gnome}}
 
 COPY system /
 COPY installers /installers
--- installers/desktop/install.sh
+++ installers/desktop/install.sh
@@ -5,18 +5,21 @@ set -ouex pipefail
 DEFAULT_BASE_IMAGE=$(grep -oP "ARG BASE_IMAGE=\"\${BASE_IMAGE:-\K[^\"]+}" Containerfile)
 BASE_IMAGE=${BASE_IMAGE:-$DEFAULT_BASE_IMAGE}
 
-echo "Installing ${BASE_IMAGE}-${RELEASE_TAG}..."
+echo "Using BASE_IMAGE » ${BASE_IMAGE}..."
 
 wget -qO- https://git.io/papirus-icon-theme-install | sh
 wget -qO- https://git.io/papirus-folders-install | sh
 
 papirus-folders -C yaru
 
+KDE_PACKAGES=(
+  kvantum
+  breeze-gtk
+)
+
 LAYERED_PACKAGES=(
   vinyl-theme
-  breeze-gtk
   alacritty
-  kvantum
   gparted
   lazygit
   neovim
@@ -24,5 +27,10 @@ LAYERED_PACKAGES=(
   code
 )
 
-# Install packages
+# Only install KDE packages if not using GNOME variant
+if [[ "$IS_GNOME_VARIANT" != "1" ]]; then
+  dnf5 install -y "${KDE_PACKAGES[@]}"
+fi
+
+# Install layered packages
 dnf5 install -y "${LAYERED_PACKAGES[@]}"
